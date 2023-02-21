b2b2eadde532e49b77b477a6372123b618f6b701
Maksimova Kristina <kris.maks.t.7@gmail.ru>
2023-02-21 09:03:05 +0300

    Добавлена кнопка и изображение Изменен цвет изображения и цвета кнопки Добавлены действия по кнопке

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var skyImageView: UIImageView!
+    @IBOutlet weak var changeButton: UIButton!
+    private var isSun = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        skyImageView.image = UIImage(systemName: "moon")
+        skyImageView.tintColor = .gray
+        
+        changeButton.backgroundColor = .green
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        if isSun {
+            skyImageView.image = UIImage(systemName: "moon")
+        } else {
+            skyImageView.image = UIImage(systemName: "sun.max")
+        }
+        isSun.toggle()
+    }
+    
+    @IBAction func buttonTouchDown(_ sender: Any) {
+        print("Нажали и не оторвали")
+    }
 }
 
